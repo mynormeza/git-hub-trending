cc750b8d6b1b96475958c4af28704e0f6669f040
Mynor Meza <mynormeza92@gmail.com>
2020-09-18 16:17:54 -0600

    feat(Domain module): set up and finsh domain module

---
+++ Domain/src/main/java/com/mynormeza/domain/interactor/bookmark/UnbookmarkProject.kt
@@ -0,0 +1,27 @@
+package com.mynormeza.domain.interactor.bookmark
+
+import com.mynormeza.domain.executor.PostExecutionThread
+import com.mynormeza.domain.interactor.CompletableUseCase
+import com.mynormeza.domain.repository.ProjectsRepository
+import io.reactivex.Completable
+import java.lang.IllegalArgumentException
+import javax.inject.Inject
+
+open class UnbookmarkProject @Inject constructor(
+    private val projectsRepository: ProjectsRepository,
+    postExecutionThread: PostExecutionThread
+) : CompletableUseCase<UnbookmarkProject.Params>(postExecutionThread) {
+
+    override fun buildUseCaseCompletable(params: Params?): Completable {
+        if (params == null) throw IllegalArgumentException("Params cannot be null")
+        return projectsRepository.unbookmarkProject(params.projectId)
+    }
+
+    data class Params constructor(val projectId: String) {
+        companion object {
+            fun forProject(projectId: String): Params {
+                return Params(projectId)
+            }
+        }
+    }
+}
