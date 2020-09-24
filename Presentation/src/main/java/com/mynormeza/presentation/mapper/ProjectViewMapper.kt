9a2c2a82420a8bb7b5857dce8ff9ec1008715cdd
Mynor Meza <mynormeza92@gmail.com>
2020-09-24 11:05:28 -0600

    add tests for presentation module

--- Presentation/src/main/java/com/mynormeza/presentation/mapper/ProjectViewMapper.kt
+++ Presentation/src/main/java/com/mynormeza/presentation/mapper/ProjectViewMapper.kt
@@ -4,7 +4,7 @@ import com.mynormeza.domain.model.Project
 import com.mynormeza.presentation.model.ProjectView
 import javax.inject.Inject
 
-class ProjectViewMapper @Inject constructor() : Mapper<ProjectView, Project> {
+open class ProjectViewMapper @Inject constructor() : Mapper<ProjectView, Project> {
 
     override fun mapToView(type: Project): ProjectView {
         return ProjectView(type.id, type.name, type.fullName,
